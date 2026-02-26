ecc28b2c3ddffdff3d883320a07eba72406efee6
AndreaBrunoo <andrea.bruno.06.ge@gmail.com>
2026-02-26 11:28:06 +0100

    json

---
+++ EserciziJson/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Net;
+using Newtonsoft.Json;
+
+//esercizio 1
+
+string contenutoJson = File.ReadAllText(@"lastId.json");
+
+var id = JsonConvert.DeserializeObject<dynamic>(contenutoJson);
+
+int ultimoId = id.lastId;
+
+var nuovoPartecipante = new
+{
+    id = ultimoId + 1,
+    nome = $"Partecipante {ultimoId + 1}"
+};
+
+string nuovoPartecipanteJson = JsonConvert.SerializeObject(nuovoPartecipante, Formatting.Indented);
+
+File.WriteAllText(@"partecipante.json", nuovoPartecipanteJson);
+
+id.lastId = ultimoId + 1;
+
+string idAggiornato = JsonConvert.SerializeObject(id, Formatting.Indented);
+File.WriteAllText(@"lastId.json", idAggiornato);
+
+//esercizio 2
+while (true)
+{
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine().Trim();
+    Console.Write("Età: ");
+    if (int.TryParse(Console.ReadLine(), out int età))
+    {
+        Console.Write("Presente (true/false)");
+        if (bool.TryParse(Console.ReadLine().ToLower(), out bool presenza))
+        {
+            var partecipante = new
+            {
+                nome = 
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Non è stato possibile aggiungere la presenza.");
+            Console.WriteLine("Assicurati che sia composta da true o false.");
+            continue;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Non è stato possibile aggiungere l'eta.");
+        Console.WriteLine("Assicurati che sia composta da soli numeri.");
+        continue;
+    }
+}
--- JSON/Program.cs
+++ JSON/Program.cs
@@ -1,14 +1,62 @@
-﻿//Esempio di file JSON semplice
+﻿
+using Newtonsoft.Json;
+
+/*
+    Comando per la libreria Newtonsoft.Json
+    dotnet add package Newtonsoft.Json
+*/
+
+// File json semplice 
 {
-    "nome": "Partecipante 1",
-    "età" : 30,
-    "presente" : true,
-    "interessi" : ["programmazzione", "musica","sport"]
+"nome": "Partecipante 1",
+"eta": 30,
+"presente": true
 }
 
+// File json complesso 
 {
-    "nome": "Partecipante 1",
-    "età" : 30,
-    "presente" : true,
-    "interessi" : ["programmazzione", "musica","sport"]
+"nome": "Partecipante 1",
+"eta": 30,
+"presente": true,
+"interessi": ["programmazione", "musica", "sport"]
 }
+
+// File json con oggetto
+{
+"nome": "Partecipante 1",
+"eta": 30,
+"presente": true,
+"interessi": ["programmazione", "musica", "sport"],
+"indirizzo": {
+    "via": "Via Roma",
+    "citta": "Milano",
+    "cap": "20100"
+  }
+}
+
+// Elenco di oggetti json
+[
+  {
+    "nome": "Partecipante 1",
+    "eta": 30,
+    "presente": true,
+    "interessi": ["programmazione", "musica", "sport"],
+    "indirizzo": {
+        "via": "Via Roma",
+        "citta": "Milano",
+        "cap": "20100"
+      }
+  },
+  {
+    "nome": "Partecipante 2",
+    "eta": 25,
+    "presente": false,
+    "interessi": ["cinema", "viaggi", "cucina"],
+    "indirizzo": {
+        "via": "Via Milano",
+        "citta": "Roma",
+        "cap": "00100"
+      }
+  }
+]
+
